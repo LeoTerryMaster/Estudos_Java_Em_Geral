0ff201b7e7f7e33f5ab67cc2409e31ee785631c8
terrygomes <leandrogomes.santos@hotmail.com>
2024-04-08 09:02:00 -0300

    Aula de varArgs

--- Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/calculadora2/Calcular.java
+++ Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/calculadora2/Calcular.java
@@ -2,7 +2,7 @@ package DevDojo.poo.orientacaoobjeto.calculadora2;
 
 public class Calcular{
 
-	public  void alteraDoisNumeros(int num1,int num2){
+	public void alteraDoisNumeros(int num1,int num2){
 		num1 = 99;
 		num2 = 33;
 		System.out.println("Dentro do ateraDosNumeros");
@@ -10,5 +10,23 @@ public class Calcular{
 		System.out.println("num2 " + num2);
 	}
 
+
+	public void somarArray(int[] numeros){
+		int soma = 0;
+		for (int i : numeros){
+			soma += i;
+		}
+		System.out.println("Soma de Arrays " + soma);
+	}
+
+
+	public void somaVarArgs(int... numeros){
+
+		int soma = 0;
+		for (int i : numeros){
+			soma += i;
+		}
+		System.out.println("Soma de VarArgs " + soma);
+	}
 
 }
--- Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/calculadora2/test/CalcularTest.java
+++ Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/calculadora2/test/CalcularTest.java
@@ -4,17 +4,34 @@ import DevDojo.poo.orientacaoobjeto.calculadora2.Calcular;
 
 public class CalcularTest{
 
-	
-	
+
 	public static void main(String[] args){
-	Calcular cal= new Calcular();
-	int num1= 1;
-	int num2= 2;
-	cal.alteraDoisNumeros(num1, num2);
-	
-	System.out.println(num1);
-	System.out.println(num2);
+		Calcular cal = new Calcular();
+		int num1 = 1;
+		int num2 = 2;
+		cal.alteraDoisNumeros(num1, num2);
+
+		System.out.println(num1);
+		System.out.println(num2);
+
+
+		int[] numeros = {
+			1,
+			2,
+			2,
+			3,
+			5,
+			4,
+			65,
+			45,};
+
+		cal.somarArray(numeros);
+
 		
+		cal.somaVarArgs(numeros);
 		
+		cal.somaVarArgs(1, 2, 2, 2, 3, 5, 445, 74, 897, 98);
+
 	}
+
 }
