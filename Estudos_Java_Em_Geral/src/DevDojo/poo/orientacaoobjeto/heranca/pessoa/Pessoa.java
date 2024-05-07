f29c8a5845db933cb92cc5b03bc2f34d0d3b4679
terrygomes <leandrogomes.santos@hotmail.com>
2024-05-07 13:48:01 -0300

    Herança

---
+++ Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/heranca/pessoa/Pessoa.java
@@ -0,0 +1,54 @@
+package DevDojo.poo.orientacaoobjeto.heranca.pessoa;
+
+public class Pessoa{
+
+	private String nome;
+	private String cpf;
+	private Endereco endereco;
+
+//	public Pessoa(String nome,String cpf){
+//		this.nome = nome;
+//		this.cpf = cpf;
+//	}
+
+	
+	
+	
+	public void imprimir() {
+		
+		System.out.println("==========================");
+		System.out.println(this.nome);
+		System.out.println(this.cpf);
+		System.out.println(this.endereco.getRua());
+		System.out.println(this.endereco.getCep());
+		System.out.println("==========================");
+		
+	}
+	public String getNome(){
+		return nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getCpf(){
+		return cpf;
+	}
+
+	public void setCpf(String cpf){
+		this.cpf = cpf;
+	}
+
+	
+	public Endereco getEndereco(){
+		return endereco;
+	}
+
+	
+	public void setEndereco(Endereco endereco){
+		this.endereco = endereco;
+	}
+
+
+}
--- Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/seminarios/Seminario.java
+++ Estudos_Java_Em_Geral/src/DevDojo/poo/orientacaoobjeto/seminarios/Seminario.java
@@ -2,59 +2,54 @@ package DevDojo.poo.orientacaoobjeto.seminarios;
 
 import DevDojo.poo.orientacaoobjeto.seminarios.test.Alunos;
 
-public class Seminario{
+public class Seminario {
 
-	private String titulos;
+    private String titulos;
 
-	private Alunos[] alunos;
-	private Local local;
+    private Alunos[] alunos;
+    private Local local;
 
-	public Seminario(String titulos){
-		this.titulos = titulos;
-	}
-
-
-	public Seminario(String titulos,Alunos[] alunos){
-		this.titulos = titulos;
-		this.alunos = alunos;
-	}
+    public Seminario(String titulos, Local local) {
+        this.titulos = titulos;
+        this.local = local;
+    }
 
 
-	public Seminario(String titulos,Alunos[] alunos,Local local){
-		this.titulos = titulos;
-		this.alunos = alunos;
-		this.local = local;
-	}
+    public Seminario(String titulos, Alunos[] alunos, Local local) {
+        this.titulos = titulos;
+        this.alunos = alunos;
+        this.local = local;
+    }
 
 
-	public String getTitulos(){
-		return titulos;
-	}
+    public String getTitulos() {
+        return titulos;
+    }
 
 
-	public void setTitulos(String titulos){
-		this.titulos = titulos;
-	}
+    public void setTitulos(String titulos) {
+        this.titulos = titulos;
+    }
 
 
-	public Alunos[] getAlunos(){
-		return alunos;
-	}
+    public Alunos[] getAlunos() {
+        return alunos;
+    }
 
 
-	public void setAlunos(Alunos[] alunos){
-		this.alunos = alunos;
-	}
+    public void setAlunos(Alunos[] alunos) {
+        this.alunos = alunos;
+    }
 
 
-	public Local getLocal(){
-		return local;
-	}
+    public Local getLocal() {
+        return local;
+    }
 
 
-	public void setLocal(Local local){
-		this.local = local;
-	}
+    public void setLocal(Local local) {
+        this.local = local;
+    }
 
 
 }
