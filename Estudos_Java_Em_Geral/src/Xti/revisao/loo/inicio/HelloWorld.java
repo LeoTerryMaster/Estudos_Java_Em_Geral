aa82ee248e2a03af367c465290c902b922e7ce22
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-10-25 09:00:26 -0300

    reset

--- Estudos_Java_Em_Geral/src/Xti/revisao/loo/inicio/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Xti.revisao.loo.inicio;
-
-
-public class HelloWorld{
-
-	public static void main(String[] args){
-		System.out.println("olá mundo, vamos fazer revisão para ficar fera...");
-	}
-}
