43e37a307a24f8e7b369773721c8e04d1d386ca6
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-10-30 08:19:29 -0300

    comentários

---
+++ src/ProfNelio/lp/comentarios/Main.java
@@ -0,0 +1,19 @@
+package ProfNelio.lp.comentarios;
+
+public class Main{
+
+
+	/*
+	 * Comntário em bloco
+	 * */
+
+
+	/**
+	 * comentário de métodos 
+	 * @param args
+	 */
+	public static void main(String[] args){
+		System.out.println("Olá comentários ");// comentários de linha
+	}
+
+}
