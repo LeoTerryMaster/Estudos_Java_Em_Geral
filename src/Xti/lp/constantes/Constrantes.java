174887be7c5e03b4f1e2b123677ce852774e7ad9
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-02 11:34:20 -0300

    CONSTANTES XTI

---
+++ src/Xti/lp/constantes/Constrantes.java
@@ -0,0 +1,18 @@
+package Xti.lp.constantes;
+
+public class Constrantes{
+
+
+	public static void main(String[] args){
+
+
+		// constantes não pode ser alteradas a não ser que o programador altere seu valor.
+		//CONSTANTES A REGRA É SEMPRE ESPREVER TODA EM MAIÚSCULAS 
+		final int IDADE = 100;
+
+		System.out.println(IDADE);
+
+
+	}
+
+}
