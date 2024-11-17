bf10536a1430876e7351385e2696521e853888f3
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-17 10:14:05 -0300

    operadores Relacionais

---
+++ src/Dod3r/lp/operadores/Relacionais.java
@@ -0,0 +1,19 @@
+package Dod3r.lp.operadores;
+
+public class Relacionais{
+
+
+	public static void main(String[] args){
+
+
+		System.out.println(21 == 22);
+		System.out.println(21 >= 22);
+		System.out.println(22 >= 22);
+		System.out.println(21 < 22);
+		System.out.println(21 <= 22);
+		System.out.println(21 != 22);
+
+
+	}
+
+}
