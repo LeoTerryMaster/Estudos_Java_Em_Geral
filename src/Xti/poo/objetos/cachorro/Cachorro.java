6697407f1ee05e7d49b7e11425ce3e9178a433f5
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-18 10:05:40 -0300

    Orientação a objetos

---
+++ src/Xti/poo/objetos/cachorro/Cachorro.java
@@ -0,0 +1,15 @@
+package Xti.poo.objetos.cachorro;
+
+public class Cachorro{
+
+
+	public String nome;
+	public int tamanho;
+	public String raca;
+
+
+	public void latir(){
+		System.out.println("O cachorro " + nome + " da raça " + raca + " e tamanho " + tamanho + " Latiu .....  Au, au, au, au");
+	}
+
+}
