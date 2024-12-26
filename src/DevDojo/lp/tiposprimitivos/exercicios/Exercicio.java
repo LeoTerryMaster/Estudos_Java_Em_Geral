5b8b8863d522453f5200e124d97403201c8a9f16
{-TerryMaster-} <165042760+LeoTerryMaster@users.noreply.github.com>
2024-12-26 06:44:58 -0300

    resumo period

---
+++ src/DevDojo/lp/tiposprimitivos/exercicios/Exercicio.java
@@ -0,0 +1,17 @@
+package DevDojo.lp.tiposprimitivos.exercicios;
+
+public class Exercicio {
+
+    static  String nome = "Leandro";
+    static  String endereco = "Rua Regina nº 300";
+    static  int salario = 5000;
+    static  String data= "30/07/2024";
+
+
+    public static void main(String[] args) {
+
+        String resposta ="Eu " + nome+ " Moro no endereço "+endereco+ " e recebi o saslário de "+salario+" na data "+data ;
+        System.out.println(resposta);
+
+    }
+}
