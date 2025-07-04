33e554da7496ee5281f303655832681ac186a539
{-TerryMaster-} <165042760+LeoTerryMaster@users.noreply.github.com>
2025-07-03 22:38:43 -0300

    if & else

---
+++ src/XTI/src/logica/Fluxo/FluxosIfElse.java
@@ -0,0 +1,20 @@
+package XTI.src.logica.Fluxo;
+
+public class FluxosIfElse{
+
+	public static void main(String[] args){
+
+		int numero = 3;
+
+
+		if((numero % 2 == 0) ){
+			System.out.println("É par");
+		}else{
+			System.out.println("É ímpar");
+
+		}
+
+
+	}
+
+}
