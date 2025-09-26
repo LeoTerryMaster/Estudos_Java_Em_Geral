3f3439afaf1356ead3935461ccdf69313af07098
TerryMaster <TerryMaster@Leandro>
2025-09-26 19:16:21 -0300

    operadores de comparação

---
+++ src/XTI/src/lp/operadorcomparacao/OperadorDeComapracao.java
@@ -0,0 +1,21 @@
+package XTI.src.lp.operadorcomparacao;
+
+public class OperadorDeComapracao{
+
+
+	public static void main(String[] args){
+
+		int idade = 41;
+
+
+		System.out.println(idade == 10 );
+		System.out.println(idade != 10);
+		System.out.println(idade > 10);
+		System.out.println(idade < 10);
+		System.out.println(idade <= 10);
+		System.out.println(idade >= 10);
+
+
+	}
+
+}
