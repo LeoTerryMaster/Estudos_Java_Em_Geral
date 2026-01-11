e30ff7697ce9ab25a0d62749cd1ba304d6f3f12c
Terry <leandrogomes.santos.terrymaster@gmail.com>
2026-01-11 15:33:57 -0300

    composição

--- src/XTI/src/poo/construtores/Motor.java
+++ src/XTI/src/poo/construtores/Motor.java
@@ -1,27 +1,27 @@
 package XTI.src.poo.construtores;
 
-
 public class Motor{
-	
+
 	public String tipo;
 	public int potencia;
 	public double cilindrada;
-	
-	
+
+
 	public Motor(){
-		
+
 	}
-	
-		public Motor( String tipo, int potencia, double cilindrada){
+
+	public Motor(String tipo,int potencia,double cilindrada){
 		this.tipo = tipo;
 		this.potencia = potencia;
 		this.cilindrada = cilindrada;
-		
+
 	}
-		
+
 	@Override
 	public String toString(){
 
 		return "Tipo: " + tipo + "\nPotência: " + potencia + " cv\nCilindrada: " + cilindrada + " L";
 	}
+
 }
